4c1f300b6c83eb2fdb4639731aa0b081ae26bfd0
Lu Hao <haolu1125@gmail.com>
2021-02-03 22:09:46 -0500

    apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,38 @@
 // We are linking our routes to a series of "data" sources.
 const fs = require("fs");
 const notes = require('../db/db.json');
+const router = require('express').Router();
+
 
 // ROUTING
 
-module.exports = (app) => {
-    // API GET Requests
+//module.exports = (app) => {
+// API GET Requests
+//responds with notes from db.json through Get "/api/notes"
+router.get('/api/notes', (req, res) => {
 
-    app.get('/api/notes', (req, res) => res.json(notes))
-};
+    notes.getNotes().then((data) => res.json(data))
+        //try to find a way to use .catch()
+        .catch((error) => res.status(500).json(error))
+})
+
+
+router.post('/api/notes', (req, res) => {
+
+    notes.saveNote(req.body).then((data) => res.json(data))
+        .catch((error) => res.status(500).json(error))
+
+})
+
+
+router.delete("/api/notes/:id", (req, res) => {
+    notes.deleteNote(req.params.id).then(() => res.json())
+        .catch((error) => res.status(500).json(error))
+
+})
+
+
+//};
 
 
 
@@ -20,25 +44,26 @@ module.exports = (app) => {
 // (ex. User fills out a reservation request... this data is then sent to the server...
 // Then the server saves the data to the tableData array)
 // ---------------------------------------------------------------------------
-app.post('/api/notes', (req, res) => {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
+// app.post('/api/notes', (req, res) => {
+//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+//     // It will do this by sending out the value "true" have a table
+//     // req.body is available since we're using the body parsing middleware
 
 
-    const newNotes = req.body;
+//     const newNotes = req.body;
 
-    console.log(newNotes);
+//     console.log(newNotes);
 
-    notes.push(newNotes);
+//     notes.push(newNotes);
 
-    res.json(notes);
+//     res.json(notes);
 
-    fs.writeFileSync("../db/db.json", JSON.stringify(notes), function (err) {
-        if (err) throw (err);
-    });
-
-
-});
+//     fs.writeFileSync("../db/db.json", JSON.stringify(notes), function (err) {
+//         if (err) throw (err);
+//     });
 
 
+// });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,16 +17,16 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use("/assets", express.static("./assets"));
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-//require('./routes/apiRoutes')(app);
-//require('./routes/htmlRoutes')(app);
+// require('./routes/apiRoutes')(app);
+// require('./routes/htmlRoutes')(app);
 app.use('/', htmlRoutes);
 app.use("/api", apiRoutes);
-
+//app.use(express.static(__dirname + '/assets/css'));
 
 // LISTENER
 // The below code effectively "starts" our server
